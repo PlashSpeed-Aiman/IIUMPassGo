9e3846fd639fc98ebf62c38d0215ee126184cc39
PlashSpeed-Aiman <62431177+PlashSpeed-Aiman@users.noreply.github.com>
2022-12-12 23:39:48 +0800

    Update main.go
    Removed references to user32.dll and the likes

--- main.go
+++ main.go
@@ -133,22 +133,3 @@ func connect_network() {
 
 }
 
-// MessageBox of Win32 API.
-func MessageBox(hwnd uintptr, caption, title string, flags uint) int {
-	ret, _, _ := syscall.NewLazyDLL("user32.dll").NewProc("MessageBoxW").Call(
-		uintptr(hwnd),
-		uintptr(unsafe.Pointer(syscall.StringToUTF16Ptr(caption))),
-		uintptr(unsafe.Pointer(syscall.StringToUTF16Ptr(title))),
-		uintptr(flags))
-
-	return int(ret)
-}
-
-// MessageBoxPlain of Win32 API.
-func MessageBoxPlain(title, caption string) int {
-	const (
-		NULL  = 0
-		MB_OK = 0
-	)
-	return MessageBox(NULL, caption, title, MB_OK)
-}
